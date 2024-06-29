89339e71e78e24189d7721238cafe3cd76c00d33
Medicopter117 <oppro.netzwerk@gmail.com>
2024-06-29 20:41:50 +0200

    Version: 1.0.0 addet Bot System

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# oppronet
+# OPPRO.NET Bot
+Der Oppro.net Bot ist ein Bot des Netzwerk Oppro.net
---
+++ main.py
@@ -0,0 +1,47 @@
+import discord
+from discord.ext import commands
+from discord.commands import slash_command, Option
+import os
+import asyncio
+import re  # regex library
+import colorama
+from colorama import Fore
+from dotenv import load_dotenv
+import ezcord
+intents = discord.Intents.default()
+intents.members = True  # Stelle sicher, dass du die richtigen Intents für mitgliederbezogene Ereignisse hast
+
+bot = discord.Bot(
+    intents=intents
+)
+
+# Ban-Befehl
+
+bot = commands.Bot(command_prefix="!")  # Beispiel-Prefix, ändern Sie es entsprechend Ihrer Konfiguration
+
+
+# NOT THIS EDIT
+
+if __name__ == "__main__":
+    for filename in os.listdir("cog"):
+        if filename.endswith(".py"):
+            bot.load_extension(f"cog.{filename[:-3]}")
+
+
+async def delayed_print(message, delay):
+    await asyncio.sleep(delay)
+    print(message)
+    # delete the is okay :(
+async def main():
+    await delayed_print(Fore.LIGHTRED_EX + "OPPRO.NET#0000", 0.5)
+    await delayed_print(Fore.LIGHTBLUE_EX + "OPPRO.NET#0000", 0.5)
+    await delayed_print(Fore.LIGHTWHITE_EX + "OPPRO.NET#0000", 0.5)
+    await delayed_print(Fore.LIGHTYELLOW_EX + "OPPRO.NET#0000", 0.5)
+    await delayed_print(Fore.LIGHTMAGENTA_EX + "OPPRO.NET Development", 1.5)
+    await delayed_print("                   ", 0)
+    await delayed_print(Fore.LIGHTCYAN_EX + "This Setup has created by LennyPegauOfficial", 0.9)
+    await delayed_print("                   ", 0)
+asyncio.run(main())
+# Bot System
+load_dotenv()
+bot.run(os.getenv("TOKEN"))
