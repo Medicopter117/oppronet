f2a0c0720842d95a8831afd139eb2f112067d66f
TheWever <85771814+TheWever@users.noreply.github.com>
2024-08-10 15:57:10 +0200

    PEP 8
    Refactored code to align with PEP 8 standards
    Added type hints
    Removed unused imports
    Renamed cog class from new1 to News1

--- cog/news.py
+++ cog/news.py
@@ -3,11 +3,13 @@ from discord.ext import commands
 from discord import slash_command, Option
 import sqlite3
 
+
 def get_db_connection():
     conn = sqlite3.connect('channels_id_newslog.db')
     return conn
 
-class news1(commands.Cog):
+
+class News1(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
@@ -18,15 +20,20 @@ class news1(commands.Cog):
         ctx,
         title: Option(str, "hier kommt der Title hin"),
         text: Option(str, "Hier kommt die Beschreibung hin"),
-        channel: Option(discord.TextChannel, "Hier kommt der Channel hin")):
-
+        channel: Option(discord.TextChannel, "Hier kommt der Channel hin")
+    ):
         embed = discord.Embed(
             title=f"{title}",
             color=discord.Color.red()
         )
         embed.set_author(name="OPPRO.NET GmbH")
         embed.add_field(name="", value=f"{text}", inline=False)
-        embed.add_field(name="Links:", value="[Invite](https://discord.com/oauth2/authorize?client_id=1248265007381348403&permissions=8&integration_type=0&scope=bot) | [Support Server](https://discord.gg/3rbVWaRTpD)")
+        embed.add_field(
+            name="Links:",
+            value="[Invite](https://discord.com/oauth2/authorize?"
+                  "client_id=1248265007381348403&permissions=8&integration_type=0&scope=bot) | "
+                  "[Support Server](https://discord.gg/3rbVWaRTpD)"
+        )
         embed.set_footer(text="Created by Oppro.net Development")
 
         await channel.send(embed=embed)
@@ -37,9 +44,15 @@ class news1(commands.Cog):
         )
         embed.set_author(name="OPPRO.NET GmbH")
         embed.add_field(name="mit dem Inhalt:", value=f"{text}", inline=False)
-        embed.add_field(name="Links:", value="[Invite](https://discord.com/oauth2/authorize?client_id=1248265007381348403&permissions=8&integration_type=0&scope=bot) | [Support Server](https://discord.gg/3rbVWaRTpD)")
+        embed.add_field(
+            name="Links:",
+            value="[Invite](https://discord.com/oauth2/authorize?"
+                  "client_id=1248265007381348403&permissions=8&integration_type=0&scope=bot) | "
+                  "[Support Server](https://discord.gg/3rbVWaRTpD)")
         embed.set_footer(text="Created by Oppro.net Development")
 
         await ctx.respond(embed=embed, ephemeral=True)
+
+
 def setup(bot):
-    bot.add_cog(news1(bot))
+    bot.add_cog(News1(bot))
