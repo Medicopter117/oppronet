68583e98a2956d8eff2f9733b3ef4918b697a27d
TheWever <85771814+TheWever@users.noreply.github.com>
2024-08-10 15:50:00 +0200

    PEP 8
    Refactored code to align with PEP 8 standards
    Fixed typos
    Added type hints
    Removed unused imports

--- cog/allgemein.py
+++ cog/allgemein.py
@@ -1,42 +1,7 @@
-import discord
-from discord.ext import commands
-from discord import slash_command, Option
 from discord.commands import SlashCommandGroup
+from discord.ext import commands
+import discord
 
-class infos(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
-    allgemein = SlashCommandGroup("allgemein")
-    @allgemein.command(description="Infomationen")
-    async def infos(self, ctx):
-
-        embed = discord.Embed(
-            title="Infomationen für den Bot",
-            color=discord.Color.red()
-        )
-        embed.set_author(name="OPPRO.NET GmbH")
-        embed.add_field(name="Coder", value="Coder: LennyPegauOfficial und Oppro.net Development")
-        embed.add_field(name="Name: OPPRO.NET Manage", value="", inline=False)
-        embed.add_field(name="Codes:", value="Admin Tools - from CodingKeks\nucreatepassword - from CodingKeks\nFeedback - From Oppro.net Development\ninfos - From Oppro.net Development\nnews - From Oppro.net Development\nstatus - From Oppro.net Development\ntimeout - From Codingkeks\nuserinfo - from Codingkeks")
-        embed.add_field(name="Rechte: ", value="Admin", inline=False)
-        embed.set_footer(text="Projekt des Oppro.net Development")
-        await ctx.respond(embed=embed, view=Button())
-
-    @allgemein.command(description="Bekomme den Support Server!")
-    async def support(self, ctx):
-
-        embed = discord.Embed(
-            title="Unser Support Server",
-            color=discord.Color.red()
-        )
-        embed.set_author(name="Oppro.net GmbH")
-        embed.add_field(name="Link: https://discord.gg/k38huukRcB", value="")
-        embed.set_footer(text="Projekt des Oppro.net Development")
-        await ctx.respond(embed=embed, ephemeral=True)
-
-
-def setup(bot):
-    bot.add_cog(infos(bot))
 
 class Button(discord.ui.View):
     @discord.ui.button(label="Links", style=discord.ButtonStyle.primary)
@@ -50,3 +15,50 @@ class Button(discord.ui.View):
         embed2.set_footer(text="Projekt des Oppro.net Development")
 
         await interactions.response.send_message(embed=embed2)
+
+
+class Infos(commands.Cog):
+    allgemein = SlashCommandGroup("allgemein")
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @allgemein.command(description="Informationen")
+    async def infos(self, ctx: discord.ApplicationContext):
+        embed = discord.Embed(
+            title="Informationen für den Bot",
+            color=discord.Color.red()
+        )
+        embed.set_author(name="OPPRO.NET GmbH")
+        embed.add_field(name="Coder", value="Coder: LennyPegauOfficial und Oppro.net Development")
+        embed.add_field(name="Name: OPPRO.NET Manage", value="", inline=False)
+        embed.add_field(
+            name="Codes:",
+            value="Admin Tools - from CodingKeks\n"
+                  "ucreatepassword - from CodingKeks\n"
+                  "Feedback - From Oppro.net Development\n"
+                  "infos - From Oppro.net Development\n"
+                  "news - From Oppro.net Development\n"
+                  "status - From Oppro.net Development\n"
+                  "timeout - From Codingkeks\nuserinfo - from Codingkeks"
+        )
+        embed.add_field(name="Rechte: ", value="Admin", inline=False)
+        embed.set_footer(text="Projekt des Oppro.net Development")
+
+        await ctx.respond(embed=embed, view=Button())
+
+    @allgemein.command(description="Bekomme den Support Server!")
+    async def support(self, ctx: discord.ApplicationContext):
+        embed = discord.Embed(
+            title="Unser Support Server",
+            color=discord.Color.red()
+        )
+        embed.set_author(name="Oppro.net GmbH")
+        embed.add_field(name="Link: https://discord.gg/k38huukRcB", value="")
+        embed.set_footer(text="Projekt des Oppro.net Development")
+
+        await ctx.respond(embed=embed, ephemeral=True)
+
+
+def setup(bot):
+    bot.add_cog(Infos(bot))
