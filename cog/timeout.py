dac8dab27b053227d1dee2030d93434a6bc9931b
TheWever <85771814+TheWever@users.noreply.github.com>
2024-08-10 16:55:11 +0200

    PEP 8
    Refactored code to align with PEP 8 standards
    Added type hints
    Removed unused imports
    Removed long chained if statements

--- cog/timeout.py
+++ cog/timeout.py
@@ -1,19 +1,19 @@
-import discord
-from discord.ext import commands
 from discord.commands import slash_command, Option
+from discord.ext import commands
 from datetime import timedelta
+import discord
 
 
-class timeout(commands.Cog):
+class Timeout(commands.Cog):
     def __int__(self, bot):
         self.bot = bot
 
     @slash_command(description="Timeoute einen Member")
     @discord.default_permissions(moderate_members=True)
     async def timeout(
-            self, ctx,
+            self,
+            ctx: discord.ApplicationContext,
             member: Option(discord.Member, "Wähle ein Member aus"),
-
             reason: Option(str, "Wähle ein Grund aus", choices=([
                 "Extremes Chatverhalten | Mute - 2 Stunden",
                 "Extreme Beleidigungen | Mute - 1 Woche",
@@ -23,24 +23,25 @@ class timeout(commands.Cog):
                 "Support-Missbrauch | Mute - 1 Tag",
             ]), required=True)
     ):
-
-        if reason == "Extremes Chatverhalten | Mute - 2 Stunden ":
-            duration = timedelta(hours=2)
-
-        elif reason == "Extreme Beleidigungen | Mute - 1 Woche":
-            duration = timedelta(weeks=1)
-
-        elif reason == "Missachtung von Team Anweisungen | Mute - 1 Tag":
-            duration = timedelta(days=1)
-
-        elif reason == "Sensible Themen | Mute - 4 Tage":
-            duration = timedelta(days=4)
-
-        elif reason == "Extreme Provokation | Mute 1 Woche":
-            duration = timedelta(weeks=1)
-
-        elif reason == "Support-Missbrauch | Mute - 1 Tag":
-            duration = timedelta(days=1)
+        match reason:
+            case "Extremes Chatverhalten | Mute - 2 Stunden":
+                duration = timedelta(hours=2)
+            case "Extreme Beleidigungen | Mute - 1 Woche":
+                duration = timedelta(weeks=1)
+            case "Missachtung von Team Anweisungen | Mute - 1 Tag":
+                duration = timedelta(days=1)
+            case "Sensible Themen | Mute - 4 Tage":
+                duration = timedelta(days=4)
+            case "Extreme Provokation | Mute 1 Woche":
+                duration = timedelta(weeks=1)
+            case "Support-Missbrauch | Mute - 1 Tag":
+                duration = timedelta(days=1)
+            case _:
+                return discord.Embed(
+                    title="✅ | User wurde erfolgreich getimeoutet",
+                    description=f"",
+                    color=discord.Color.green()
+                )
 
         try:
             await member.timeout_for(duration, reason=reason)
@@ -50,41 +51,43 @@ class timeout(commands.Cog):
                             f"\nGrund: {reason}",
                 color=discord.Color.green()
             )
-            em = discord.Embed(
-                title="⛔ | Fehler beim muten",
-                description="Ich habe keine Berechtigung, um diesen Member zu muten",
-                color=discord.Color.red()
+        except (AttributeError, discord.Forbidden):
+            return await ctx.respond(
+                embed=discord.Embed(
+                    title="⛔ | Fehler beim muten",
+                    description="Ich habe entweder keine Berechtigung, "
+                                "um diesen Member zu muten oder dieser User ist nicht Teil dieses Servers",
+                    color=discord.Color.red()
+                ),
+                ephemeral=True
             )
-        except discord.Forbidden:
-            await ctx.respond(embed=em, ephemeral=True)
-            return
+
         await ctx.respond(embed=embed)
 
     @slash_command(description="Entferne einen Timeout")
     @discord.default_permissions(moderate_members=True)
-    async def removetimeout(
+    async def remove_timeout(
             self, ctx,
             member: Option(discord.Member, "Wähle einen Member"),
     ):
         try:
-            await member.timeout(None)
+            await member.Timeout(None)
             embed = discord.Embed(
                 title="✅ | User wurde erfolgreich entmuted",
                 description=f"Der Member {member.mention} wurde entmuted",
                 color=discord.Color.green()
             )
-
-            em = discord.Embed(
-                title="⛔ | Fehler beim entmuten",
-                description="Ich habe keine Berechtigung, um diesen Member zu entmuten",
-                color=discord.Color.red()
-            )
-
         except discord.Forbidden:
-            await ctx.respond(embed=em, ephemeral=True)
-            return
+            return await ctx.respond(
+                embed=discord.Embed(
+                    title="⛔ | Fehler beim entmuten",
+                    description="Ich habe keine Berechtigung, um diesen Member zu entmuten",
+                    color=discord.Color.red()
+                ),
+                ephemeral=True
+            )
         await ctx.respond(embed=embed)
 
 
 def setup(bot):
-    bot.add_cog(timeout(bot))
+    bot.add_cog(Timeout(bot))
