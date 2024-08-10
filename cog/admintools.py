ead3ff0eb330f3b60c819c0f5a15c4f879dd0bc7
TheWever <85771814+TheWever@users.noreply.github.com>
2024-08-10 15:48:52 +0200

    PEP 8, removed unban command
    Refactored code to align with PEP 8 standards
    Fixed typos
    Removed unused imports
    Removed unban command since user is not displayed for member option after being banned

--- cog/admintools.py
+++ cog/admintools.py
@@ -1,13 +1,10 @@
-import discord
-from discord import Embed, Color
-from discord.commands import slash_command, Option
-from discord.ext import commands
 from discord.commands import SlashCommandGroup
-
+from discord.commands import Option
+from discord.ext import commands
 import aiosqlite
-import datetime
-
 import traceback
+import datetime
+import discord
 
 
 class ModerationSystem(commands.Cog):
@@ -40,29 +37,29 @@ class ModerationSystem(commands.Cog):
     @discord.guild_only()
     async def kick(
             self,
-            ctx,
+            ctx: discord.ApplicationContext,
             member: Option(discord.Member, "Wähle den User aus, den du kicken willst", required=True),
             reason: Option(str, "Gib einen Grund an, warum du den User kicken willst", required=False,
                            default="Kein Grund angegeben")
     ):
-
         kick_embed = discord.Embed(
             title=f"`✅` Kick {member.name}#{member.discriminator}",
             description=f"Du hast den User {member.mention} aus dem Server **{ctx.guild.name}** gekickt.",
             color=discord.Color.green(),
             timestamp=datetime.datetime.utcnow()
         )
-        kick_embed.add_field(name="Moderator:", value=f"{ctx.author}", inline=False)
+        kick_embed.add_field(name="Moderator:", value=f"{ctx.user}", inline=False)
         kick_embed.add_field(name="Grund:", value=f"{reason}", inline=False)
         kick_embed.set_author(name=f"{ctx.guild.name}", icon_url=member.avatar.url)
         kick_embed.set_thumbnail(url=member.avatar.url)
-        kick_embed.set_footer(text=f"{ctx.bot.user.name}#{ctx.bot.user.discriminator} | Oppro.net Development",
-                              icon_url=ctx.bot.user.avatar.url)
+        kick_embed.set_footer(
+            text=f"{self.bot.user.name}#{self.bot.user.discriminator} | Oppro.net Development",
+            icon_url=self.bot.user.avatar.url
+        )
 
         try:
             await member.kick(reason=reason)
-        except (discord.Forbidden, discord.HTTPException) as e:
-
+        except (discord.Forbidden, discord.HTTPException) as error:
             error_embed = discord.Embed(
                 title="`⚠️` Error",
                 description=f"Es ist ein Fehler aufgetreten.",
@@ -71,14 +68,16 @@ class ModerationSystem(commands.Cog):
             )
             error_embed.add_field(name=f"Beim Kicken von {member.mention} ist ein Fehler aufgetreten.",
                                   value=f"Bitte versuche es später erneut.", inline=False)
-            error_embed.add_field(name=f"Fehler Code:", value=f"```{e}```", inline=False)
-            error_embed.set_author(name=f"{ctx.guild.name}", icon_url=ctx.bot.user.avatar.url)
-            error_embed.set_footer(text=f"{ctx.bot.user.name}#{ctx.bot.user.discriminator} | Oppro.net Development",
-                                   icon_url=ctx.bot.user.avatar.url)
+            error_embed.add_field(name=f"Fehler Code:", value=f"```{error}```", inline=False)
+            error_embed.set_author(name=f"{ctx.guild.name}", icon_url=self.bot.user.avatar.url)
+            error_embed.set_footer(
+                text=f"{self.bot.user.name}#{self.bot.user.discriminator} | Oppro.net Development",
+                icon_url=self.bot.user.avatar.url
+            )
 
-            print(e)
-            await ctx.respond(embed=error_embed, ephemeral=True)
-            return
+            traceback.print_exception(error)
+            return await ctx.respond(embed=error_embed, ephemeral=True)
+
         await ctx.respond(embed=kick_embed, ephemeral=False)
 
     @ban_group.command(description="Banne einen User aus dem Server")
@@ -86,12 +85,11 @@ class ModerationSystem(commands.Cog):
     @discord.guild_only()
     async def ban(
             self,
-            ctx,
+            ctx: discord.ApplicationContext,
             member: Option(discord.Member, "Wähle den User aus, den du Bannen willst", required=True),
             reason: Option(str, "Gib einen Grund an, warum du den User Bannen willst", required=False,
                            default="Kein Grund angegeben")
     ):
-
         ban_embed = discord.Embed(
             title=f"`✅` Ban {member.name}#{member.discriminator}",
             description=f"Du hast den User {member.mention} aus dem Server **{ctx.guild.name}** gebannt.",
@@ -102,12 +100,14 @@ class ModerationSystem(commands.Cog):
         ban_embed.add_field(name="Grund:", value=f"{reason}", inline=False)
         ban_embed.set_author(name=f"{ctx.guild.name}", icon_url=member.avatar.url)
         ban_embed.set_thumbnail(url=member.avatar.url)
-        ban_embed.set_footer(text=f"{ctx.bot.user.name}#{ctx.bot.user.discriminator} | Oppro.net Development", icon_url=ctx.bot.user.avatar.url)
+        ban_embed.set_footer(
+            text=f"{ctx.bot.user.name}#{ctx.bot.user.discriminator} | Oppro.net Development",
+            icon_url=ctx.bot.user.avatar.url
+        )
 
         try:
             await member.ban(reason=reason)
         except (discord.Forbidden, discord.HTTPException) as e:
-
             error_embed = discord.Embed(
                 title="`⚠️` Error",
                 description=f"Es ist ein Fehler aufgetreten.",
@@ -121,73 +121,29 @@ class ModerationSystem(commands.Cog):
             error_embed.set_footer(text=f"{ctx.bot.user.name}#{ctx.bot.user.discriminator} | Oppro.net Development",
                                    icon_url=ctx.bot.user.avatar.url)
 
-            print(e)
-            await ctx.respond(embed=error_embed, ephemeral=True)
-            return
+            traceback.print_exception(e)
+            return await ctx.respond(embed=error_embed, ephemeral=True)
+
         await ctx.respond(embed=ban_embed, ephemeral=False)
-
-    @ban_group.command(description="Entbanne einen User aus dem Server")
-    @discord.default_permissions(ban_members=True)
-    @discord.guild_only()
-    async def unban(
-            self,
-            ctx,
-            member: Option(discord.Member, "Wähle den User aus, den du entbannen willst", required=True),
-            reason: Option(str, "Gib einen Grund an, warum du den User entbannen willst", required=False,
-                           default="Kein Grund angegeben")
-    ):
-
-        unban_embed = discord.Embed(
-            title=f"`✅` Unban {member.name}#{member.discriminator}",
-            description=f"Du hast den User {member.mention} auf dem Server **{ctx.guild.name}** entbannt.",
-            color=discord.Color.green(),
-            timestamp=datetime.datetime.utcnow()
-        )
-        unban_embed.add_field(name="Moderator:", value=f"{ctx.author}", inline=False)
-        unban_embed.add_field(name="Grund:", value=f"{reason}", inline=False)
-        unban_embed.set_author(name=f"{ctx.guild.name}", icon_url=member.avatar.url)
-        unban_embed.set_thumbnail(url=member.avatar.url)
-        unban_embed.set_footer(text=f"{ctx.bot.user.name}#{ctx.bot.user.discriminator} | Oppro.net Development",
-                               icon_url=ctx.bot.user.avatar.url)
-
-        try:
-            ban_entry = await ctx.guild.fetch_ban(member)
-            await ctx.guild.unban(ban_entry.user, reason=reason)
-        except (discord.Forbidden, discord.HTTPException) as e:
-
-            error_embed = discord.Embed(
-                title="`⚠️` Error",
-                description=f"Es ist ein Fehler aufgetreten.",
-                color=discord.Color.green(),
-                timestamp=datetime.datetime.utcnow()
-            )
-            error_embed.add_field(name=f"Beim Kicken von {member.mention} ist ein Fehler aufgetreten.",
-                                  value=f"Bitte versuche es später erneut.", inline=False)
-            error_embed.add_field(name=f"Fehler Code:", value=f"```{e}```", inline=False)
-            error_embed.set_author(name=f"{ctx.guild.name}", icon_url=ctx.bot.user.avatar.url)
-            error_embed.set_footer(text=f"{ctx.bot.user.name}#{ctx.bot.user.discriminator} | Oppro.net Development",
-                                   icon_url=ctx.bot.user.avatar.url)
-
-            print(e)
-            await ctx.respond(embed=error_embed, ephemeral=True)
-            return
-        await ctx.respond(embed=unban_embed, ephemeral=False)
 
     @warn_group.command(description="Warne einen User aus dem Server")
     @discord.default_permissions(kick_members=True)
     @discord.guild_only()
     async def warn(
             self,
-            ctx,
+            ctx: discord.ApplicationContext,
             member: Option(discord.Member, "Wähle den User aus, den du warnen willst", required=True),
-            reason: Option(str, "Gib einen Grund an, warum du den User warnen willst", required=False,
-                           default="Kein Grund angegeben")
+            reason: Option(
+                str,
+                "Gib einen Grund an, warum du den User warnen willst",
+                required=False, default="Kein Grund angegeben"
+            )
     ):
-
         warn_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         async with aiosqlite.connect("mod_sys.db") as db:
             await db.execute(
-                "INSERT INTO WarnList (user_id, guild_id, warns, warn_reason, mod_id, warn_time) VALUES (?, ?, ?, ?, ?, ?)",
+                "INSERT INTO WarnList "
+                "(user_id, guild_id, warns, warn_reason, mod_id, warn_time) VALUES (?, ?, ?, ?, ?, ?)",
                 (member.id, ctx.guild.id, 1, reason, ctx.author.id, warn_time),
             )
             await db.commit()
@@ -199,19 +155,21 @@ class ModerationSystem(commands.Cog):
                 row = await cursor.fetchone()
                 warn_id = row[0]
 
-        warnUser_embed = discord.Embed(
+        warn_user_embed = discord.Embed(
             title="`⚠️` Warn",
             description=f"Du wurdest auf dem Server **{ctx.guild.name}** gewarnt.",
             color=discord.Color.green(),
             timestamp=datetime.datetime.utcnow()
         )
-        warnUser_embed.add_field(name="Moderator:", value=f"```{ctx.author}```", inline=False)
-        warnUser_embed.add_field(name="Warn ID:", value=f"```{warn_id}```", inline=False)
-        warnUser_embed.add_field(name="Grund:", value=f"```{reason}```", inline=False)
-        warnUser_embed.set_author(name=f"{ctx.guild.name}", icon_url=member.avatar.url)
-        warnUser_embed.set_thumbnail(url=member.avatar.url)
-        warnUser_embed.set_footer(text=f"{ctx.bot.user.name}#{ctx.bot.user.discriminator} | Oppro.net Development",
-                                  icon_url=ctx.bot.user.avatar.url)
+        warn_user_embed.add_field(name="Moderator:", value=f"```{ctx.author}```", inline=False)
+        warn_user_embed.add_field(name="Warn ID:", value=f"```{warn_id}```", inline=False)
+        warn_user_embed.add_field(name="Grund:", value=f"```{reason}```", inline=False)
+        warn_user_embed.set_author(name=f"{ctx.guild.name}", icon_url=member.avatar.url)
+        warn_user_embed.set_thumbnail(url=member.avatar.url)
+        warn_user_embed.set_footer(
+            text=f"{ctx.bot.user.name}#{ctx.bot.user.discriminator} | Oppro.net Development",
+            icon_url=ctx.bot.user.avatar.url
+        )
 
         warn_embed = discord.Embed(
             title="`✅` Warn",
@@ -224,10 +182,12 @@ class ModerationSystem(commands.Cog):
         warn_embed.add_field(name="Grund:", value=f"```{reason}```", inline=False)
         warn_embed.set_author(name=f"{ctx.guild.name}", icon_url=ctx.user.avatar.url)
         warn_embed.set_thumbnail(url=member.avatar.url)
-        warn_embed.set_footer(text=f"{ctx.bot.user.name}#{ctx.bot.user.discriminator} | Oppro.net Development",
-                              icon_url=ctx.bot.user.avatar.url)
+        warn_embed.set_footer(
+            text=f"{ctx.bot.user.name}#{ctx.bot.user.discriminator} | Oppro.net Development",
+            icon_url=ctx.bot.user.avatar.url
+        )
 
-        await member.send(embed=warnUser_embed)
+        await member.send(embed=warn_user_embed)
         await ctx.respond(embed=warn_embed, ephemeral=False)
 
     @warn_group.command(description="Unwarn einen User aus dem Server")
@@ -235,26 +195,30 @@ class ModerationSystem(commands.Cog):
     @discord.guild_only()
     async def unwarn(
             self,
-            ctx,
+            ctx: discord.ApplicationContext,
             member: Option(discord.Member, "Wähle den User aus, den du unwarnen willst", required=True),
             warn_id: Option(int, "Wähle die Warn ID aus, die du zurückziehen willst", required=True),
-            reason: Option(str, "Gib einen Grund an, warum du den User warnen willst", required=False,
-                           default="Kein Grund angegeben")
+            reason: Option(
+                str,
+                "Gib einen Grund an, warum du den User warnen willst",
+                required=False, default="Kein Grund angegeben"
+            )
     ):
-
-        unwarnUser_embed = discord.Embed(
+        unwarn_user_embed = discord.Embed(
             title="`🍀` Unwarn",
             description=f"Ein Warn von dir vom Server **{ctx.guild.name}** wurde zurückgezogen.",
             color=discord.Color.green(),
             timestamp=datetime.datetime.utcnow()
         )
-        unwarnUser_embed.add_field(name="Moderator:", value=f"```{ctx.author}```", inline=False)
-        unwarnUser_embed.add_field(name="Warn ID:", value=f"```{warn_id}```", inline=False)
-        unwarnUser_embed.add_field(name="Grund:", value=f"```{reason}```", inline=False)
-        unwarnUser_embed.set_author(name=f"{ctx.guild.name}", icon_url=ctx.bot.user.avatar.url)
-        unwarnUser_embed.set_thumbnail(url=ctx.guild.icon.url)
-        unwarnUser_embed.set_footer(text=f"{ctx.bot.user.name}#{ctx.bot.user.discriminator} | Oppro.net Development",
-                                    icon_url=ctx.bot.user.avatar.url)
+        unwarn_user_embed.add_field(name="Moderator:", value=f"```{ctx.author}```", inline=False)
+        unwarn_user_embed.add_field(name="Warn ID:", value=f"```{warn_id}```", inline=False)
+        unwarn_user_embed.add_field(name="Grund:", value=f"```{reason}```", inline=False)
+        unwarn_user_embed.set_author(name=f"{ctx.guild.name}", icon_url=ctx.bot.user.avatar.url)
+        unwarn_user_embed.set_thumbnail(url=ctx.guild.icon.url)
+        unwarn_user_embed.set_footer(
+            text=f"{ctx.bot.user.name}#{ctx.bot.user.discriminator} | Oppro.net Development",
+            icon_url=ctx.bot.user.avatar.url
+        )
 
         unwarn_embed = discord.Embed(
             title=f"`✅` Unwarn",
@@ -277,25 +241,27 @@ class ModerationSystem(commands.Cog):
             )
             await db.commit()
 
-        await member.send(embed=unwarnUser_embed)
+        await member.send(embed=unwarn_user_embed)
         await ctx.respond(embed=unwarn_embed, ephemeral=False)
 
     @warn_group.command(description="Zeige alle Warns eines Users aus dem Server an")
     @discord.default_permissions(kick_members=True)
     @discord.guild_only()
-    async def warnings(self, ctx, member: discord.Member):
-
+    async def warnings(self, ctx: discord.ApplicationContext, member: discord.Member):
         warns_info = []
         async with aiosqlite.connect("mod_sys.db") as db:
             async with db.execute(
-                    "SELECT warn_id, mod_id, guild_id, user_id, warns, warn_reason, warn_time FROM WarnList WHERE user_id = ? AND guild_id = ?",
+                    "SELECT warn_id, mod_id, guild_id, user_id, warns, warn_reason, warn_time "
+                    "FROM WarnList WHERE user_id = ? AND guild_id = ?",
                     (member.id, ctx.guild.id)) as cursor:
                 rows = await cursor.fetchall()
                 for row in rows:
                     warn_id, mod_id, guild_id, user_id, warns, warn_reason, warn_time = row
                     warn_time = datetime.datetime.strptime(warn_time, '%Y-%m-%d %H:%M:%S')
                     warns_info.append(
-                        f"**Warn-ID:** __{warn_id}__ | **Warn ausgestellt am:** {warn_time.strftime('%Y-%m-%d %H:%M:%S')}\n")
+                        f"**Warn-ID:** __{warn_id}__ | "
+                        f"**Warn ausgestellt am:** {warn_time.strftime('%Y-%m-%d %H:%M:%S')}\n"
+                    )
                     warns_info.append(f"**Moderator:** <@{mod_id}> | **Mod-ID**: __{mod_id}__\n")
                     warns_info.append(f"**> Grund:**\n```{warn_reason}```")
                     warns_info.append("\n")
@@ -323,11 +289,12 @@ class ModerationSystem(commands.Cog):
 
     @admin.command(description="Lösche Nachrichten aus dem Channel")
     @commands.has_permissions(administrator=True)
-    async def purge(self, ctx, amount: Option(int, "Anzahl an Nachrichten (min. 1 | max. 100)", required=True)):
-        amount = amount + 1
-
+    async def purge(
+            self,
+            ctx: discord.ApplicationContext,
+            amount: Option(int, "Anzahl an Nachrichten (min. 1 | max. 100)", required=True)
+    ):
         if amount > 101:
-
             error_embed = discord.Embed(
                 title="`❌` Fehler!",
                 description="`Ich kann nicht mehr als 100 Nachrichten Löschen!`",
@@ -337,25 +304,32 @@ class ModerationSystem(commands.Cog):
             error_embed.set_thumbnail(url=ctx.guild.icon.url)
             error_embed.set_footer(text=f"| {ctx.bot.user.name}#{ctx.bot.user.discriminator}",
                                    icon_url=ctx.bot.user.avatar.url)
-            error_embed.set_author(name=f"Purge | Moderation System | Oppro.net Development", icon_url=ctx.bot.user.avatar.url)
-
-            await ctx.respond(embed=error_embed, delete_after=6, ephemeral=True)
-
-        else:
-            deleted = await ctx.channel.purge(limit=amount)
-
-            success_embed = discord.Embed(
-                title="`✅` Erfolgreich!",
-                description="**{}** `Nachrichten gelöscht!`".format(len(deleted)),
-                color=discord.Color.green(),
-                timestamp=datetime.datetime.utcnow()
+            error_embed.set_author(
+                name=f"Purge | Moderation System | Oppro.net Development",
+                icon_url=ctx.bot.user.avatar.url
             )
-            success_embed.set_thumbnail(url=ctx.guild.icon.url)
-            success_embed.set_footer(text=f"| {ctx.bot.user.name}#{ctx.bot.user.discriminator}",
-                                     icon_url=ctx.bot.user.avatar.url)
-            success_embed.set_author(name=f"Purge | Moderation System | Oppro.net Development", icon_url=ctx.bot.user.avatar.url)
 
-            await ctx.respond(embed=success_embed, delete_after=3, ephemeral=True)
+            return await ctx.respond(embed=error_embed, delete_after=6, ephemeral=True)
+
+        deleted = await ctx.channel.purge(limit=amount)
+
+        success_embed = discord.Embed(
+            title="`✅` Erfolgreich!",
+            description="**{}** `Nachrichten gelöscht!`".format(len(deleted)),
+            color=discord.Color.green(),
+            timestamp=datetime.datetime.utcnow()
+        )
+        success_embed.set_thumbnail(url=ctx.guild.icon.url)
+        success_embed.set_footer(
+            text=f"| {ctx.bot.user.name}#{ctx.bot.user.discriminator}",
+            icon_url=ctx.bot.user.avatar.url
+        )
+        success_embed.set_author(
+            name=f"Purge | Moderation System | Oppro.net Development",
+            icon_url=ctx.bot.user.avatar.url
+        )
+
+        await ctx.respond(embed=success_embed, delete_after=3, ephemeral=True)
 
 
 def setup(bot):
