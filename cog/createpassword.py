d21b267bcc6d3db3afba4b407c3b9b77d96670e9
TheWever <85771814+TheWever@users.noreply.github.com>
2024-08-10 15:53:37 +0200

    PEP 8
    Refactored code to align with PEP 8 standards
    Added type hints
    Removed unused imports

--- cog/createpassword.py
+++ cog/createpassword.py
@@ -1,25 +1,26 @@
-import discord
+from discord.commands import slash_command
 from discord.ext import commands
-from discord.commands import slash_command, Option
+import discord
 import random
+import string
 
 
 class Password(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @slash_command(name="createpassword", description="Erstelle ein sicheres Passwort!")
+    @slash_command(name="create-password", description="Erstelle ein sicheres Passwort!")
     @discord.option("länge", int, description="Bitte wähle eine Passwort-Länge! (mind. 8 - max 12)", required=True)
-    async def createpassword(self,ctx, länge: int):
-        if länge >= 13: # Maximale Passwort-Länge +1
-            await ctx.respond("Das Passwort darf nicht länger als 12 Zeichen sein!", ephemeral=True)
-            return
-        if länge <= 7: # Mindest Passwort-Länge -1
-            await ctx.respond("Das Passwort darf nicht kürzer als 8 Zeichen sein!", ephemeral=True)
-            return
+    async def create_password(self, ctx: discord.ApplicationContext, length: int):
+        if length >= 13:
+            return await ctx.respond("Das Passwort darf nicht länger als 12 Zeichen sein!", ephemeral=True)
 
-        chars = 'abcdefghijklmnpqstuvxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!"§$%&/()=?@'
-        password = ''.join(random.choice(chars) for _ in range(länge))
+        if length <= 7:
+            return await ctx.respond("Das Passwort darf nicht kürzer als 8 Zeichen sein!", ephemeral=True)
+
+        chars: str = string.ascii_letters + string.digits + '!"§$%&/()=?@'
+        password: str = ''.join(random.choice(chars) for _ in range(length))
+
         await ctx.respond(f"Dein Passwort lautet: ||{password}||", ephemeral=True)
 
 
