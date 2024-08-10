82fa209a7bc5b2e54cc2532340dfef4757730130
TheWever <85771814+TheWever@users.noreply.github.com>
2024-08-10 16:01:24 +0200

    PEP 8, rewrote status command
    Refactored code to align with PEP 8 standards
    Added type hints
    Removed unused imports
    Optimized code by removing long if chains
    Renamed cog Userinfo to UserInfo

--- cog/userinfo.py
+++ cog/userinfo.py
@@ -4,25 +4,29 @@
 # ┃      ┃  ┣━━━━━━┛
 # ┃      ┃  ┃
 # ┗━━━━━━┛  ┃
-
-
-
-import discord
 from discord import slash_command
-import ezcord
 from ezcord import View
-class Userinfo(ezcord.Cog, emoji="<:info:1147664192325812406>"):
+from bot import Bot
+import discord
+import ezcord
 
+
+class UserInfo(ezcord.Cog, emoji="<:info:1147664192325812406>"):
     @slash_command(description="Lasse dir Informationen über einen Benutzer auf den Server anzeigen")
-    @discord.option("user",discord.Member,description="Wähle den User aus über den die Info sein soll")
-    async def user(self,ctx:discord.ApplicationContext, member:discord.Member=None):
+    @discord.option("user", discord.Member, description="Wähle den User aus über den die Info sein soll")
+    async def user(
+            self,
+            ctx: discord.ApplicationContext,
+            member: discord.Member = None
+    ):
         if member is None:
             member = ctx.author
+
         if member not in ctx.guild.members:
-            await ctx.respond("Dieser User ist nicht auf dem Server",ephemeral=True)
-            return
+            return await ctx.respond("Dieser User ist nicht auf dem Server", ephemeral=True)
+
+        activities = []
         if isinstance(member, discord.Member):  # Überprüfe, ob member ein Mitglied ist
-            activities = []
             for activity in member.activities:
                 if isinstance(activity, discord.Spotify):
                     txt = f'Spotify: [{activity.artist} - {activity.title}]({activity.track_url})'
@@ -35,11 +39,15 @@ class Userinfo(ezcord.Cog, emoji="<:info:1147664192325812406>"):
                 else:
                     txt = f'{activity.name}: {activity.details}'
                 activities.append(txt)
+
         rlist = []
         for role in member.roles:
             rlist.append(str(role.mention))
         rlist.reverse()
-        embed = discord.Embed(title=f"memberinfo über {member.display_name} <a:exclamination:1250412438281650290>", color=0x5965f2)
+        embed = discord.Embed(
+            title=f"memberinfo über {member.display_name} <a:exclamination:1250412438281650290>",
+            color=0x5965f2
+        )
         embed.add_field(name="Name:", value=f"{member}")
         embed.add_field(name="Nickname", value=f"{member.nick}" if member.nick else "hat keinen Nickname",inline=True)
         embed.add_field(name="Display Name", value=f"{member.display_name}")
@@ -55,23 +63,23 @@ class Userinfo(ezcord.Cog, emoji="<:info:1147664192325812406>"):
         embed.add_field(name="Timeout?",value=f"ja" if member.timed_out else "nein")
         embed.set_footer(text="OPPRO.NET | created by Lucky 👑")
         banner_user = await self.bot.fetch_user(member.id)
+
         try:
             embed.set_image(url=banner_user.banner.url)
         except AttributeError:
             pass
+
         if activities:
             embed.add_field(name='Aktivitäten', inline=False, value='\n'.join(activities))
+
         embed.set_author(name=f"{member}", icon_url=f"{member.display_avatar}")
         embed.set_thumbnail(url=member.display_avatar)
         embed.set_footer(text=f'Angefragt von {ctx.user.name} • {ctx.user.id}', icon_url=ctx.user.display_avatar)
         await ctx.respond(embed=embed,view=Userbutton(self,ctx,member,ctx.user))
 
 
-def setup(bot: discord.Bot):
-    bot.add_cog(Userinfo(bot))
-
 class Userbutton(View):
-    def __init__(self,bot,ctx,member,user) -> None:
+    def __init__(self, bot: Bot, ctx: discord.ApplicationContext, member: discord.Member, user: discord.User) -> None:
         self.bot = bot
         self.ctx = ctx
         self.member = member
@@ -79,7 +87,7 @@ class Userbutton(View):
         super().__init__(timeout=None)
     
     @discord.ui.button(label="🔰 Home", style=discord.ButtonStyle.green, custom_id="info")
-    async def info(self, button,ctx: discord.Interaction):
+    async def info(self, button,ctx: discord.ApplicationContext):
         if self.member is None:
             self.member = self.ctx.author
         if isinstance(self.member, discord.Member):
@@ -123,7 +131,6 @@ class Userbutton(View):
             embed.add_field(name='Aktivitäten', inline=False, value='\n'.join(activities))
         embed.set_thumbnail(url=self.member.display_avatar)
         await ctx.response.edit_message(embed=embed)
-
     
     @discord.ui.button(label="Avatar", style=discord.ButtonStyle.gray, custom_id="avatar")
     async def avatar(self, button,ctx: discord.ApplicationContext):
@@ -134,7 +141,7 @@ class Userbutton(View):
         await ctx.response.edit_message(embed=embed)
     
     @discord.ui.button(label="Banner", style=discord.ButtonStyle.blurple, custom_id="banner")
-    async def banner(self, button,ctx: discord.Interaction):
+    async def banner(self, button,ctx: discord.ApplicationContext):
         if self.user.id != ctx.user.id:
             return await ctx.respond("Du hast den Command nicht ausgeführt",ephemeral=True)
         
@@ -158,7 +165,6 @@ class Userbutton(View):
         embed.add_field(name="Berechtigungen",value=",".join([str(perm[0]) for perm in self.member.guild_permissions if perm[1]]))
         await ctx.response.edit_message(embed=embed)
 
-
     @discord.ui.button(label="Rollen", style=discord.ButtonStyle.blurple, custom_id="Rollen")
     async def rollen(self, button,ctx: discord.ApplicationContext):
         if self.user.id != ctx.user.id:
@@ -175,3 +181,7 @@ class Userbutton(View):
         embed.add_field(name=f"Rollen: {len(self.member.roles) - 1}", value=','.join(rlist),inline=False)
 
         await ctx.response.edit_message(embed=embed)
+
+
+def setup(bot: discord.Bot):
+    bot.add_cog(Userinfo(bot))
