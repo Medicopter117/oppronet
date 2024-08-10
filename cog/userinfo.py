37716dca5c9f242d8c4b1312a82daf94d68002e8
TheWever <85771814+TheWever@users.noreply.github.com>
2024-08-10 16:44:07 +0200

    PEP 8
    Added additional type hinting 
    Refactored old code

--- cog/userinfo.py
+++ cog/userinfo.py
@@ -26,7 +26,7 @@ class UserInfo(ezcord.Cog, emoji="<:info:1147664192325812406>"):
             return await ctx.respond("Dieser User ist nicht auf dem Server", ephemeral=True)
 
         activities = []
-        if isinstance(member, discord.Member):  # Überprüfe, ob member ein Mitglied ist
+        if isinstance(member, discord.Member):
             for activity in member.activities:
                 if isinstance(activity, discord.Spotify):
                     txt = f'Spotify: [{activity.artist} - {activity.title}]({activity.track_url})'
@@ -45,22 +45,25 @@ class UserInfo(ezcord.Cog, emoji="<:info:1147664192325812406>"):
             rlist.append(str(role.mention))
         rlist.reverse()
         embed = discord.Embed(
-            title=f"memberinfo über {member.display_name} <a:exclamination:1250412438281650290>",
+            title=f"MemberInfo über {member.display_name} <a:exclamination:1250412438281650290>",
             color=0x5965f2
         )
         embed.add_field(name="Name:", value=f"{member}")
-        embed.add_field(name="Nickname", value=f"{member.nick}" if member.nick else "hat keinen Nickname",inline=True)
+        embed.add_field(name="Nickname", value=f"{member.nick}" if member.nick else "hat keinen Nickname", inline=True)
         embed.add_field(name="Display Name", value=f"{member.display_name}")
         embed.add_field(name="Farbe", value=f"{member.colour}")
         embed.add_field(name="Erwähnung", value=f"{member.mention}")
         embed.add_field(name='Status:', value=member.status, inline=False)
-        embed.add_field(name="Booster", value=f"Ja<t:{int(member.premium_since.timestamp())}:F>" if member.premium_since else "Nein")
+        embed.add_field(
+            name="Booster",
+            value=f"Ja<t:{int(member.premium_since.timestamp())}:F>" if member.premium_since else "Nein"
+        )
         embed.add_field(name="Höchste Rolle", value=f"{member.top_role.mention}")
         embed.add_field(name="Bot:", value=f'{("Ja" if member.bot else "Nein")}')
         embed.add_field(name="Server beigetreten:", value=f"<t:{int(member.joined_at.timestamp())}:F>")
         embed.add_field(name="Discord beigetreten:", value=f"<t:{int(member.created_at.timestamp())}:F>")
-        embed.add_field(name=f"Rollen: {len(member.roles) - 1}", value=','.join(rlist),inline=False)
-        embed.add_field(name="Timeout?",value=f"ja" if member.timed_out else "nein")
+        embed.add_field(name=f"Rollen: {len(member.roles) - 1}", value=','.join(rlist), inline=False)
+        embed.add_field(name="Timeout?", value=f"ja" if member.timed_out else "nein")
         embed.set_footer(text="OPPRO.NET | created by Lucky 👑")
         banner_user = await self.bot.fetch_user(member.id)
 
@@ -75,10 +78,10 @@ class UserInfo(ezcord.Cog, emoji="<:info:1147664192325812406>"):
         embed.set_author(name=f"{member}", icon_url=f"{member.display_avatar}")
         embed.set_thumbnail(url=member.display_avatar)
         embed.set_footer(text=f'Angefragt von {ctx.user.name} • {ctx.user.id}', icon_url=ctx.user.display_avatar)
-        await ctx.respond(embed=embed,view=Userbutton(self,ctx,member,ctx.user))
+        await ctx.respond(embed=embed, view=UserButton(self, ctx, member, ctx.user))
 
 
-class Userbutton(View):
+class UserButton(View):
     def __init__(self, bot: Bot, ctx: discord.ApplicationContext, member: discord.Member, user: discord.User) -> None:
         self.bot = bot
         self.ctx = ctx
@@ -87,11 +90,12 @@ class Userbutton(View):
         super().__init__(timeout=None)
     
     @discord.ui.button(label="🔰 Home", style=discord.ButtonStyle.green, custom_id="info")
-    async def info(self, button,ctx: discord.ApplicationContext):
+    async def info(self, button: discord.ui.Button, ctx: discord.ApplicationContext):
         if self.member is None:
             self.member = self.ctx.author
+
+        activities = []
         if isinstance(self.member, discord.Member):
-            activities = []
             for activity in self.member.activities:
                 if isinstance(activity, discord.Spotify):
                     txt = f'Spotify: [{activity.artist} - {activity.title}]({activity.track_url})'
@@ -104,84 +108,108 @@ class Userbutton(View):
                 else:
                     txt = f'{activity.name}: {activity.details}'
                 activities.append(txt)
+
         rlist = []
         for role in self.member.roles:
             rlist.append(str(role.mention))
         rlist.reverse()
+
         embed = discord.Embed(title=f"Memberinfo über {self.member.display_name}", color=0x5965f2)
         embed.add_field(name="Name:", value=f"{self.member}")
-        embed.add_field(name="Nickname", value=f"{self.member.nick}" if self.member.nick else "hat keinen Nickname",inline=True)
+        embed.add_field(
+            name="Nickname",
+            value=f"{self.member.nick}" if self.member.nick else "hat keinen Nickname",
+            inline=True
+        )
         embed.add_field(name="Display Name", value=f"{self.member.display_name}")
         embed.add_field(name="Farbe", value=f"{self.member.colour}")
         embed.add_field(name="Erwähnung", value=f"{self.member.mention}")
         embed.add_field(name='Status:', value=self.member.status, inline=False)
-        embed.add_field(name="Booster", value=f"Ja<t:{int(self.member.premium_since.timestamp())}:F>" if self.member.premium_since else "Nein")
+        embed.add_field(
+            name="Booster",
+            value=f"Ja<t:{int(self.member.premium_since.timestamp())}:F>" if self.member.premium_since else "Nein"
+        )
         embed.add_field(name="Höchste Rolle", value=f"{self.member.top_role.mention}")
         embed.add_field(name="Bot:", value=f'{("Ja" if self.member.bot else "Nein")}')
         embed.add_field(name="Server beigetreten:", value=f"<t:{int(self.member.joined_at.timestamp())}:F>")
-        embed.add_field(name="<:Discord:1239890198674800671>  Discord beigetreten:", value=f"<t:{int(self.member.created_at.timestamp())}:F>")
-        embed.add_field(name=f"Rollen: {len(self.member.roles) - 1}", value=','.join(rlist),inline=False)
-        embed.add_field(name="Timeout?",value=f"ja" if self.member.timed_out else "nein")
-        banner_user = await ctx.client.fetch_user(self.member.id)
+        embed.add_field(
+            name="<:Discord:1239890198674800671>  Discord beigetreten:",
+            value=f"<t:{int(self.member.created_at.timestamp())}:F>"
+        )
+        embed.add_field(name=f"Rollen: {len(self.member.roles) - 1}", value=','.join(rlist), inline=False)
+        embed.add_field(name="Timeout?", value=f"ja" if self.member.timed_out else "nein")
+        embed.set_thumbnail(url=self.member.display_avatar)
+
+        banner_user = await self.bot.fetch_user(self.member.id)
         try:
             embed.set_image(url=banner_user.banner.url)
         except AttributeError:
             pass
+
         if activities:
             embed.add_field(name='Aktivitäten', inline=False, value='\n'.join(activities))
-        embed.set_thumbnail(url=self.member.display_avatar)
+
         await ctx.response.edit_message(embed=embed)
     
     @discord.ui.button(label="Avatar", style=discord.ButtonStyle.gray, custom_id="avatar")
-    async def avatar(self, button,ctx: discord.ApplicationContext):
+    async def avatar(self, button: discord.ui.Button, ctx: discord.ApplicationContext):
         if self.user.id != ctx.user.id:
-            return await ctx.respond("Du hast den Command nicht ausgeführt",ephemeral=True)
-        embed = discord.Embed(title="Avatar von {}".format(self.member.display_name))
+            return await ctx.respond("Du hast den Command nicht ausgeführt", ephemeral=True)
+        embed = discord.Embed(title=f"Avatar von {self.member.display_name}")
         embed.set_image(url=self.member.display_avatar)
         await ctx.response.edit_message(embed=embed)
     
     @discord.ui.button(label="Banner", style=discord.ButtonStyle.blurple, custom_id="banner")
-    async def banner(self, button,ctx: discord.ApplicationContext):
+    async def banner(self, button: discord.ui.Button, ctx: discord.ApplicationContext):
         if self.user.id != ctx.user.id:
-            return await ctx.respond("Du hast den Command nicht ausgeführt",ephemeral=True)
+            return await ctx.respond("Du hast den Command nicht ausgeführt", ephemeral=True)
         
         embed = discord.Embed(title="Banner von {}".format(self.member.display_name))
+        
         if self.member.bot:
-            embed.add_field(name=" ",value="Bots haben keinen Banner")
+            embed.add_field(name=" ", value="Bots haben keinen Banner")
             return await ctx.response.edit_message(embed=embed)
-        banner_user = await ctx.client.fetch_user(self.member.id)
+
+        banner_user = await self.bot.fetch_user(self.member.id)
         try:
             embed.set_image(url=banner_user.banner.url)
         except AttributeError:
-            embed.add_field(name=" ",value="Dieser User hat keinen Banner")
+            embed.add_field(name=" ", value="Dieser User hat keinen Banner")
+
         await ctx.response.edit_message(embed=embed)
 
     @discord.ui.button(label="Berechtigungen", style=discord.ButtonStyle.red, custom_id="berechtigungen")
-    async def berechtigungen(self, button,ctx: discord.ApplicationContext):
+    async def berechtigungen(self, button: discord.ui.Button, ctx: discord.ApplicationContext):
         if self.user.id != ctx.user.id:
-            return await ctx.respond("Du hast den Command nicht ausgeführt",ephemeral=True)
+            return await ctx.respond("Du hast den Command nicht ausgeführt", ephemeral=True)
         
         embed = discord.Embed(title="Berechtigungen von {}".format(self.member.display_name))
-        embed.add_field(name="Berechtigungen",value=",".join([str(perm[0]) for perm in self.member.guild_permissions if perm[1]]))
+        embed.add_field(
+            name="Berechtigungen",
+            value=",".join([str(perm[0]) for perm in self.member.guild_permissions if perm[1]])
+        )
         await ctx.response.edit_message(embed=embed)
 
     @discord.ui.button(label="Rollen", style=discord.ButtonStyle.blurple, custom_id="Rollen")
-    async def rollen(self, button,ctx: discord.ApplicationContext):
+    async def rollen(self, button: discord.ui.Button, ctx: discord.ApplicationContext):
         if self.user.id != ctx.user.id:
-            return await ctx.respond("Du hast den Command nicht ausgeführt",ephemeral=True)
-        rlist = []
-        for role in self.member.roles:
-            if role.name == "@everyone":
-                continue
-            rlist.append(str(role.mention))
-        embed = discord.Embed(title="Rollen von {}".format(self.member.display_name))
-        if len(rlist) == 0:
-            embed.add_field(name="Rollen",value="Dieser User hat keine Rollen")
+            return await ctx.respond("Du hast den Command nicht ausgeführt", ephemeral=True)
+
+        embed = discord.Embed(title=f"Rollen von {self.member.display_name}")
+        rlist = [
+            role.mention
+            for role in self.member.roles
+            if role.name == "@everyone"
+        ]
+
+        if not rlist:
+            embed.add_field(name="Rollen", value="Dieser User hat keine Rollen")
             return await ctx.response.edit_message(embed=embed)
-        embed.add_field(name=f"Rollen: {len(self.member.roles) - 1}", value=','.join(rlist),inline=False)
+
+        embed.add_field(name=f"Rollen: {len(self.member.roles) - 1}", value=','.join(rlist), inline=False)
 
         await ctx.response.edit_message(embed=embed)
 
 
 def setup(bot: discord.Bot):
-    bot.add_cog(Userinfo(bot))
+    bot.add_cog(UserInfo(bot))
